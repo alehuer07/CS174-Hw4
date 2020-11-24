31dd4d46a677d7f4bb7035f7d2f0d36558149667
Alexis Huerta <alehuer90@gmail.com>
2020-11-23 23:05:21 -0800

    create landing page upload form

--- src/views/LandingView.php
+++ src/views/LandingView.php
@@ -12,31 +12,31 @@ class LandingView extends View{
 <html>
 
 <head>
-    <title>Move Reviews</title>
+    <title>Community Jigsaw</title>
 </head>
 
 <body>
-    <div>
-        <div>
-            <h1 style="font-size:20pt;"><a href=".">Movie Reviews</a></h1>
-            <div>
-                <h2>Genres</h2>
-                <ul>
-                    <li><b>[<a href=".">New Genre</a>]</b></li>
-                    <li><b><a href=".">Action</a></b></li>
-                    <li><b><a href=".">Comedy</a></b></li>
-                    <li><b><a href=".">Drama</a></b></li>
-                    <li><b><a href=".">Horror</a></b></li>
-                    <li><b><a href=".">Musical</a></b></li>
-                    <li><b><a href=".">Romance</a> [<a href=".">-</a>]</b></li>
-                </ul>
-            </div>
-            <div style="height:1in;">
-                <h2 style="margin-bottom:1px;padding-bottom:1px;">Reviews</h2>
-                <ul style="margin:1px;padding:1px;">
-                    <li><a href=".">Sonic Lives!!</a> 2020-10-13</li>
-                    <li><a href=".">What's Tenet spelt backwards?</a> 2020-10-11</li>
-                    <li><a href=".">Don't have a First Cow</a> 2020-10-08</li>
+    <div class="container" style="margin:auto;">
+        <div class="imageUpload">
+            <form style="float:left;">
+                <label for="fname">New Image:</label>
+                <input type="text" id="newImage" name="newImage">
+                <input type="submit" value="Upload">
+
+            </form>
+        </div>
+        <div class="grid">
+            <div id="piece1"></div>
+            <div id="piece2"></div>
+            <div id="piece3"></div>
+            <div id="piece4"></div>
+            <div id="piece5"></div>
+            <div id="piece6"></div>
+            <div id="piece7"></div>
+            <div id="piece8"></div>
+            <div id="piece9"></div>
+        </div>
+    </div>
 </body>
 
 </html><?php
